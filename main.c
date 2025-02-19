86168075ebc495f6e21e41197b079323dcbd7d3a
Douglas S. Elias <douglassantoselias@gmail.com>
2025-02-19 11:12:02 -0300

    Add thread to actor struct

--- main.c
+++ main.c
@@ -25,10 +25,12 @@ struct Actor {
   u64 state;
 
   compute_t* compute;
+  HANDLE thread;
 };
 
 u64 global_actor_id = 0;
 Actor* global_actor_list[100] = {0};
+DWORD run_actor(void* args);
 
 Actor* create_actor(u64 mailbox_capacity, compute_t* compute) {
   Actor* actor = calloc(sizeof(Actor), 1);
@@ -39,7 +41,13 @@ Actor* create_actor(u64 mailbox_capacity, compute_t* compute) {
   actor->id = global_actor_id;
   global_actor_list[actor->id] = actor;
   global_actor_id++;
+
+  actor->thread = CreateThread(NULL, 0, run_actor, (void*)actor, CREATE_SUSPENDED, NULL);
   return actor;
+}
+
+void init_actor(Actor* actor) {
+  ResumeThread(actor->thread);
 }
 
 DWORD run_actor(void* args) {
@@ -55,42 +63,42 @@ DWORD run_actor(void* args) {
   return 0;
 }
 
-void send_message(Actor* actor, char* message) {
+void send_message(Actor* actor, u8* message, u64 message_size) {
   u64 write_index = InterlockedCompareExchange64(&actor->write_index, (actor->write_index + 1) % actor->mailbox_capacity, actor->write_index);
-  strcpy(actor->mailbox[write_index], message);
+  memcpy(actor->mailbox[write_index], message, message_size);
   InterlockedIncrement64(&(actor->total_messages));
 }
 
 /// User defined
-void compute_state_a(Actor* actor, void* message) {
-  char* msg = (char*)message;
-  printf("Computing state -> Actor ID: %lld MSG: %s\n", actor->id, msg);
+void compute_state_a(Actor* actor, void* message_arg) {
+  char* message_received = (char*)message_arg;
+  printf("Computing state -> Actor ID: %lld MSG: %s\n", actor->id, message_received);
   actor->state++;
-  send_message(global_actor_list[actor->id+1], "Hello next actor");
+  char* message = "Hello next actor";
+  send_message(global_actor_list[actor->id+1], (u8*)message, strlen(message));
 }
 
-void compute_state_b(Actor* actor, void* message) {
-  char* msg = (char*)message;
-  printf("Computing state -> Actor ID: %lld MSG: %s\n", actor->id, msg);
+void compute_state_b(Actor* actor, void* message_arg) {
+  char* message_received = (char*)message_arg;
+  printf("Computing state -> Actor ID: %lld MSG: %s\n", actor->id, message_received);
   actor->state++;
-  send_message(global_actor_list[actor->id-1], "Hello previous actor");
+  char* message = "Hello previous actor";
+  send_message(global_actor_list[actor->id-1], (u8*)message, strlen(message));
 }
 
 int main() {
-  Actor* tom_cruise = create_actor(100, &compute_state_a);
-  Actor* jim_carrey = create_actor(100, &compute_state_b);
-
-  send_message(tom_cruise, "Hello Tom.");
+  Actor* tom_cruise = create_actor(100, compute_state_a);
+  Actor* jim_carrey = create_actor(100, compute_state_b);
   
-  HANDLE tom_cruise_thread = CreateThread(NULL, 0, run_actor, (void*)tom_cruise, 0, NULL);
-  HANDLE jim_carrey_thread = CreateThread(NULL, 0, run_actor, (void*)jim_carrey, 0, NULL);
+  char* message = "Hello Tom.";
+  send_message(tom_cruise, (u8*)message, strlen(message));
 
-  // run_actor(tom_cruise);
-  // run_actor(jim_carrey);
+  init_actor(tom_cruise);
+  init_actor(jim_carrey);
 
-  int total_threads = 2;
+  s32 total_threads = 2;
   HANDLE threads[2] = {0};
-  threads[0] = tom_cruise_thread;
-  threads[1] = jim_carrey_thread;
+  threads[0] = tom_cruise->thread;
+  threads[1] = jim_carrey->thread;
   WaitForMultipleObjects(total_threads, threads, true, INFINITE);
 }
